f0bba3f2486c4e9d661a10dc19921b6770cdf409
Jonas Oliveira <jonascsoliveira@outlook.com>
2021-06-15 20:34:31 -0300

    Order Entity

---
+++ src/main/java/com/example/ecomerce/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.example.ecomerce.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ecomerce.entities.Order;
+import com.example.ecomerce.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new EntityNotFoundException("None entity with id: " + id + " was found!"));
+	}
+}
