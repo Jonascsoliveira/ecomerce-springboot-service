d675985e951b81d214b0109816264d55fde4ca2f
Jonas Oliveira <jonascsoliveira@outlook.com>
2021-06-15 21:52:24 -0300

    Category

---
+++ src/main/java/com/example/ecomerce/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.example.ecomerce.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ecomerce.entities.Category;
+import com.example.ecomerce.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new EntityNotFoundException("None entity with id: " + id + " was found!"));
+	}
+}
